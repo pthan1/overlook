9fd20b669debb4e59041f077083502da6554863c
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-09-25 20:38:42 -0600

    Create UserModel

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,5 +1,20 @@
+import db from '../apiCalls';
 class UserModel {
-  constructor() {}
+  constructor() {
+    this.allUsers = [];
+    this.currentUser = {};
+  }
+
+  getAllCustomers() {
+    this.allUsers = db.getAllCustomers();
+    return db.getAllCustomers();
+  }
+
+  getSingleCustomer(userId) {
+    this.currentUser = db.getSingleCustomer(userId);
+    return db.getSingleCustomer(userId);
+  }
+
 }
 
 export default UserModel;
