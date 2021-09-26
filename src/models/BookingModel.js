f2a681c0f7d1c222ece83ea0a62c79e284309eec
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-09-25 20:45:23 -0600

    Create BookingModel

--- src/models/BookingModel.js
+++ src/models/BookingModel.js
@@ -1,7 +1,28 @@
+import db from '../apiCalls';
+
 class BookingModel {
   constructor() {
-    
+    this.allBookings = [];
   }
+
+  getAllBookings() {
+    this.allBookings = db.getAllBookings;
+    return db.getAllBookings;
+  }
+
+  addNewBooking(booking) {
+    db.addNewBooking(booking);
+    //updateDOMsomehow
+    //figure out what view i'm doing this in, and what that view looks like    
+  }
+
+  deleteSingleBooking(bookingId) {
+    db.deleteSingleBooking(bookingId);
+    //updateDOMsomehow
+    //figure out what view i'm doing this in, and what that view looks like    
+    // Maybe on the dashboard booking card.  Add a delete button.  Attach that button's value to the booking id, and call this handler.
+  }
+
 }
 
 export default BookingModel;
