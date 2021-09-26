b689acdbae4f71b079d26481c4d5c8ec0bf7d347
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-09-26 12:49:13 -0600

    Update test

--- test/UserModel-test.js
+++ test/UserModel-test.js
@@ -1,8 +1,15 @@
 import chai from 'chai';
+import UserModel from '../src/models/UserModel';
 const expect = chai.expect;
 
 describe('UserModel', function() {
-  it('', function () {
+  let userModel;
+  
+  beforeEach(function() {
+    userModel = new UserModel();
+  })
+  
+  it('should get all customers', function () {
     expect(true).to.equal(true);
   });
 
