8ebbacb67265b4cdf5412c90f5c149b253911781
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-09-26 22:01:34 -0600

    Import images

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,16 @@
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+
+import './images/bg.jpg'
+import './images/double-bed.jpg'
+import './images/junior-suite.jpg'
+import './images/logo-large.png'
+import './images/logo-small.png'
+import './images/residential-suite.jpg'
+import './images/single-bed.jpg'
+
+
 
 import UserController from './controllers/UserController';
 import BookingController from './controllers/BookingController';
